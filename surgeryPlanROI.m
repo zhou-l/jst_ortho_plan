08e38c958abfd986aa5cc44826368c370d00996c
zhou-l <31491660+zhou-l@users.noreply.github.com>
2020-12-11 00:46:29 +0800

    Add files via upload

--- surgeryPlanROI.m
+++ surgeryPlanROI.m
@@ -94,140 +94,112 @@ plot([eBotVert(2),eBotVert(4)],[eBotVert(1),eBotVert(3)],'b', 'LineWidth', 2);
 
 
 %% 5. find the intersection of two vertical axes
-vAxLower = eBotVert;
-vAxUpper = eTopVert;
-hAxLower = eBot;
-hAxUpper = eTop;
+% convert the line segments [ys xs yt xt] to [pt.y pt.x vy vx] 
+ptAxLower = [0.5 .* (eBotVert(2)+eBotVert(4)), 0.5 .* (eBotVert(1)+eBotVert(3))];
+dirAxLower = normalize( [eBotVert(2) - eBotVert(4), eBotVert(1) - eBotVert(3)], 'norm', 2);
+vAxLower = [ptAxLower, dirAxLower];
 
-dx = vAxLower(2) - vAxUpper(2);
-dy = vAxLower(1) - vAxUpper(1);
+ptVAxUpper = [ 0.5 .* (eTopVert(2)+eTopVert(4)), 0.5 .* (eTopVert(1)+eTopVert(3))];
+dirVAxUpper = normalize( [eTopVert(2) - eTopVert(4), eTopVert(1) - eTopVert(3)], 'norm', 2);
+vAxUpper = [ptVAxUpper dirVAxUpper];
+
+ptHAxLower = [0.5 .* (eBot(2)+eBot(4)), 0.5 .* (eBot(1)+eBot(3))];
+dirHAxLower = normalize([eBot(2) - eBot(4), eBot(1) - eBot(3)], 'norm', 2);
+hAxLower = [ptHAxLower,dirHAxLower];
+
+ptHAxUpper = [0.5 .* (eTop(2)+eTop(4)),0.5 .* (eTop(1)+eTop(3))];
+dirHAxUpper = normalize([eTop(2) - eTop(4), eTop(1) - eTop(3)], 'norm', 2);
+hAxUpper = [ptHAxUpper,dirHAxUpper];
+
+% find the intersection of two vertical axes
+dx = vAxLower(1) - vAxUpper(1);
+dy = vAxLower(2) - vAxUpper(2);
 det = vAxLower(3) * vAxUpper(4) - vAxLower(4) * vAxUpper(3);
 if abs(det) < 1e-8
     warning('This is a rare case that two axes do not intersect! Quit!');
     return ;
 end
+
 % find the intersection point
-u = (dy * vAxLower(4) - dx * vAxLower(3)) / det; 
-v = (dy * vAxUpper(4) - dx * vAxUpper(3)) / det;
-
-intPt = [vAxLower(1) + v * vAxLower(3),vAxLower(2) + v * vAxLower(4)]
-plot(intPt(2),intPt(1), 'ro');
-
-% % draw horizontal axis at this point
-hPt0 = [intPt(2)-scale* hAxUpper(:,4), intPt(1)-scale* hAxUpper(:,3)];
-hPt1 = [intPt(2)+scale* hAxUpper(:,4), intPt(1)+scale* hAxUpper(:,3)];
+u = (dy * vAxLower(3) - dx * vAxLower(4)) / det; 
+v = (dy * vAxUpper(3) - dx * vAxUpper(4)) / det;
+scale = 300;
+intPt = [vAxLower(1) + v * vAxLower(3),vAxLower(2) + v * vAxLower(4)];
+% draw horizontal axis at this point
+hPt0 = [intPt(1)-scale* hAxUpper(:,3), intPt(2)-scale* hAxUpper(:,4)];
+hPt1 = [intPt(1)+scale* hAxUpper(:,3), intPt(2)+scale* hAxUpper(:,4)];
 plot([hPt0(1),hPt1(1)], [hPt0(2), hPt1(2)], 'LineWidth', 3);
 
-%%4. find the bottom vertical line
-% 
-% I_xRayInput = Ifull;
-% [Gmag, Gdir] = imgradient(Ifull,'sobel');
-% imshow(Gmag);
-% % alpha(hI, 0.7);[
-% 
-% 
-% scale = 600;
-% 
-% % plot required axes
-% % upper 
-% plot([hAxUpper(:,1)-scale .* hAxUpper(:,3), hAxUpper(:,1)+scale .* hAxUpper(:,3)],... 
-%      [hAxUpper(:,2)-scale .* hAxUpper(:,4), hAxUpper(:,2)+scale .* hAxUpper(:,4)], 'LineWidth', 2);
-% plot([vAxUpper(:,1)-scale .* vAxUpper(:,3), vAxUpper(:,1)+scale .* vAxUpper(:,3)],...
-%      [vAxUpper(:,2)-scale .* vAxUpper(:,4), vAxUpper(:,2)+scale .* vAxUpper(:,4)], 'LineWidth', 2);
-% % lower
-% plot([hAxLower(:,1)-scale .* hAxLower(:,3), hAxLower(:,1)+scale .* hAxLower(:,3)],...
-%      [hAxLower(:,2)-scale .* hAxLower(:,4), hAxLower(:,2)+scale .* hAxLower(:,4)], 'LineWidth', 2);
-% plot([vAxLower(:,1)-scale .* vAxLower(:,3), vAxLower(:,1)+scale .* vAxLower(:,3)],...
-%      [vAxLower(:,2)-scale .* vAxLower(:,4), vAxLower(:,2)+scale .* vAxLower(:,4)], 'LineWidth', 2);
-%  
-% % find the intersection of two vertical axes
-% dx = vAxLower(1) - vAxUpper(1);
-% dy = vAxLower(2) - vAxUpper(2);
-% det = vAxLower(3) * vAxUpper(4) - vAxLower(4) * vAxUpper(3);
-% if abs(det) < 1e-8
-%     warning('This is a rare case that two axes do not intersect! Quit!');
-%     return ;
-% end
-% 
-% % find the intersection point
-% u = (dy * vAxLower(3) - dx * vAxLower(4)) / det; 
-% v = (dy * vAxUpper(3) - dx * vAxUpper(4)) / det;
-% 
-% intPt = [vAxLower(1) + v * vAxLower(3),vAxLower(2) + v * vAxLower(4)];
-% % draw horizontal axis at this point
-% hPt0 = [intPt(1)-scale* hAxUpper(:,3), intPt(2)-scale* hAxUpper(:,4)];
-% hPt1 = [intPt(1)+scale* hAxUpper(:,3), intPt(2)+scale* hAxUpper(:,4)];
-% plot([hPt0(1),hPt1(1)], [hPt0(2), hPt1(2)], 'LineWidth', 3);
-% 
-% % find intersection of the horizontal line and the boundary
-% baselineIntPts = [];
-% for i = 1:length(kUpper)-1
-%     bPt0 = [boundUpper(kUpper(i),2), boundUpper(kUpper(i),1)];
-%     bPt1 = [boundUpper(kUpper(i+1),2), boundUpper(kUpper(i+1),1)];
-%     if intersectLL2D(hPt0, hPt1, bPt0, bPt1)
-%         lines = [hPt0; hPt1; bPt0; bPt1];
-%         pt = linlinintersect(lines);
-%         baselineIntPts(end+1,:) = pt;
-%         plot(pt(1), pt(2), 'o', 'LineWidth', 2);
-%     end
-% end
-% 
-% if isempty(baselineIntPts)
-%     disp('Cannot find cut intersection point!');
-%     return;
-% end
-% % draw cut path
-% cutEntryPt = baselineIntPts(1,:);
-% if size(baselineIntPts,1) == 2
-%     if baselineIntPts(1,1) > baselineIntPts(2,1)  % element 1 is on the right 
-%         if zIsRightInImg
-%             cutEntryPt = baselineIntPts(1,:);
-%             cutExitPt = baselineIntPts(2,:);
-%         else
-%             cutEntryPt = baselineIntPts(2,:);
-%             cutExitPt = baselineIntPts(1,:);
-%         end
-%     else
-%         if zIsRightInImg
-%             cutEntryPt = baselineIntPts(2,:);
-%             cutExitPt = baselineIntPts(1,:);
-%         else
-%             cutEntryPt = baselineIntPts(1,:);
-%             cutExitPt = baselineIntPts(2,:);
-%         end
-%     end
-% end
-% % The cutting ray: 
-% if zIsRightInImg
-% %     Rcy = cutEntryPt + t * sin(zalpha/180*pi);
-% %     Rcx = cutEntryPt - t * cos(zalpha/180*pi);
-%       Rc = [cutEntryPt(1) cutEntryPt(2) -cos(zStretchAlpha/180*pi) -sin(zStretchAlpha/180*pi) ];
-% else
-% %     Rcy = cutEntryPt + t * sin(zalpha/180*pi);
-% %     Rcx = cutEntryPt + t * cos(zalpha/180*pi);  
-%     Rc = [cutEntryPt(1) cutEntryPt(2)  cos(zStretchAlpha/180*pi) -sin(zStretchAlpha/180*pi)];
-% end
-% % draw the cutting ray
-% cPt0 = [cutEntryPt(1)-scale* Rc(:,3), cutEntryPt(2)-scale* Rc(:,4)];
-% cPt1 = [cutEntryPt(1)+scale* Rc(:,3), cutEntryPt(2)+scale* Rc(:,4)];
-% plot([cPt0(1),cPt1(1)], [cPt0(2), cPt1(2)], 'LineWidth', 3);
-% 
-% cutTurnPt = cutExitPt;
-% for i = 1:length(kUpper)-1
-%     bPt0 = [boundUpper(kUpper(i),2), boundUpper(kUpper(i),1)];
-%     bPt1 = [boundUpper(kUpper(i+1),2), boundUpper(kUpper(i+1),1)];
-%     if intersectLL2D(cPt0, cPt1, bPt0, bPt1)
-%         lines = [cPt0; cPt1; bPt0; bPt1];
-%         pt = linlinintersect(lines);
-%         
-%         if norm(pt - cutEntryPt) < 1e-4
-%             continue;
-%         else
-%             cutTurnPt = pt;
-%             plot(pt(1), pt(2), 'o', 'LineWidth', 2);
-%         end
-%     end
-% end
-% 
-% % connect the cut path: entry -> turn -> exit pts
-% plot([cutEntryPt(1) cutTurnPt(1) cutExitPt(1)], [cutEntryPt(2) cutTurnPt(2) cutExitPt(2)], 'r', 'LineWidth', 3);
+
+% find intersection of the horizontal line and the boundary
+baselineIntPts = [];
+for i = 1:length(kUpper)-1
+    bPt0 = [boundUpper(kUpper(i),2), boundUpper(kUpper(i),1)];
+    bPt1 = [boundUpper(kUpper(i+1),2), boundUpper(kUpper(i+1),1)];
+    if intersectLL2D(hPt0, hPt1, bPt0, bPt1)
+        lines = [hPt0; hPt1; bPt0; bPt1];
+        pt = linlinintersect(lines);
+        baselineIntPts(end+1,:) = pt;
+        plot(pt(1), pt(2), 'o', 'LineWidth', 2);
+    end
+end
+
+if isempty(baselineIntPts)
+    disp('Cannot find cut intersection point!');
+    return;
+end
+% draw cut path
+cutEntryPt = baselineIntPts(1,:);
+if size(baselineIntPts,1) == 2
+    if baselineIntPts(1,1) > baselineIntPts(2,1)  % element 1 is on the right 
+        if zIsRightInImg
+            cutEntryPt = baselineIntPts(1,:);
+            cutExitPt = baselineIntPts(2,:);
+        else
+            cutEntryPt = baselineIntPts(2,:);
+            cutExitPt = baselineIntPts(1,:);
+        end
+    else
+        if zIsRightInImg
+            cutEntryPt = baselineIntPts(2,:);
+            cutExitPt = baselineIntPts(1,:);
+        else
+            cutEntryPt = baselineIntPts(1,:);
+            cutExitPt = baselineIntPts(2,:);
+        end
+    end
+end
+% The cutting ray: 
+if zIsRightInImg
+%     Rcy = cutEntryPt + t * sin(zalpha/180*pi);
+%     Rcx = cutEntryPt - t * cos(zalpha/180*pi);
+      Rc = [cutEntryPt(1) cutEntryPt(2) -cos(zStretchAlpha/180*pi) -sin(zStretchAlpha/180*pi) ];
+else
+%     Rcy = cutEntryPt + t * sin(zalpha/180*pi);
+%     Rcx = cutEntryPt + t * cos(zalpha/180*pi);  
+    Rc = [cutEntryPt(1) cutEntryPt(2)  cos(zStretchAlpha/180*pi) -sin(zStretchAlpha/180*pi)];
+end
+% draw the cutting ray
+cPt0 = [cutEntryPt(1)-scale* Rc(:,3), cutEntryPt(2)-scale* Rc(:,4)];
+cPt1 = [cutEntryPt(1)+scale* Rc(:,3), cutEntryPt(2)+scale* Rc(:,4)];
+plot([cPt0(1),cPt1(1)], [cPt0(2), cPt1(2)], 'LineWidth', 3);
+
+cutTurnPt = cutExitPt;
+for i = 1:length(kUpper)-1
+    bPt0 = [boundUpper(kUpper(i),2), boundUpper(kUpper(i),1)];
+    bPt1 = [boundUpper(kUpper(i+1),2), boundUpper(kUpper(i+1),1)];
+    if intersectLL2D(cPt0, cPt1, bPt0, bPt1)
+        lines = [cPt0; cPt1; bPt0; bPt1];
+        pt = linlinintersect(lines);
+        
+        if norm(pt - cutEntryPt) < 1e-4
+            continue;
+        else
+            cutTurnPt = pt;
+            plot(pt(1), pt(2), 'o', 'LineWidth', 2);
+        end
+    end
+end
+
+% connect the cut path: entry -> turn -> exit pts
+plot([cutEntryPt(1) cutTurnPt(1) cutExitPt(1)], [cutEntryPt(2) cutTurnPt(2) cutExitPt(2)], 'r', 'LineWidth', 3);
